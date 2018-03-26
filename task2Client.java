5c86805e67dce580574f4fe93cb2bbbb7318d97f
Josh <joshua.hansen188@gmail.com>
2018-03-26 20:34:58 +1100

    added comments

--- task2Client.java
+++ task2Client.java
@@ -8,28 +8,41 @@ class task2Client
 {
     public static void main(String[] args)
     {
+        //server Host name
         String serverHostname = "127.0.0.1";
+        //server ports
         int serverPort = 10007;
         int cksumServerPort = 10008;
+        //display details for each server port
         displayDetails(serverPort);
+        displayDetails(cksumServerPort);
         try {
+            //create ServerSocket connections
             Socket echoSocket = new Socket(serverHostname, serverPort);
             Socket cksumSocket = new Socket(serverHostname, cksumServerPort);
+            //create wirters to send data to server
             PrintWriter output = new PrintWriter(echoSocket.getOutputStream(), true);
             PrintWriter cksumOutput = new PrintWriter(cksumSocket.getOutputStream(), true);
+            //create buffers for keyboard and server inputs
             BufferedReader input = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
             InputStream keyboardInputStream = System.in;
             BufferedReader keyboardInput = new BufferedReader(new InputStreamReader(keyboardInputStream));
+            //create checksum for input from keyboard
             CheckedInputStream cksum = new CheckedInputStream(keyboardInputStream, new CRC32());
             String userInput;
+            //while client inputs data loop
             while ((userInput = keyboardInput.readLine()) != null)
             {
+                //print clients input and calculate checksum
                 System.out.println("Client: " + userInput);
                 cksum.getChecksum().update(userInput.getBytes(), 0, userInput.length());
                 System.out.println("Checksum: " + cksum.getChecksum().getValue());
+                //send data to server
                 output.println(userInput);
+                //send checksum to server
                 cksumOutput.println(cksum.getChecksum().getValue());
                 String server = input.readLine();
+                //if server responds with terminating 'X' character break loop 
                 if(server.equals("X"))
                 {
                     System.out.println("Server: " + server);
@@ -39,6 +52,7 @@ class task2Client
                 else
                     System.out.println("Server: " + server);
             }
+            //close connection to server
             output.close();
             input.close();
             keyboardInput.close();
@@ -49,7 +63,7 @@ class task2Client
             System.out.println("Error with input/output: " + ex);
         }
     }
-    
+    //display details for a port number
     public static void displayDetails(int port)
     {
         try {
@@ -64,6 +78,7 @@ class task2Client
             System.out.println("Falled to get Network Interface details: " + ex);
         }
     }
+    //display host name and address
     public static void displayAddress(NetworkInterface netIf)
     {
             Enumeration<InetAddress> inetAddresses = netIf.getInetAddresses();
--- task2Server.java
+++ task2Server.java
@@ -8,37 +8,55 @@ class task2Server
 {
     public static void main(String[] args) throws IOException
     {
-        ServerSocket serverSocket = null;
+        //port for server client to transmit data
         int serverPort = 10007;
+        //port for client to send checksum
         int cksumServerPort = 10008;
-        serverSocket = new ServerSocket(serverPort);
+        //create ServerSocket connections
+        ServerSocket serverSocket = new ServerSocket(serverPort);
         ServerSocket cksumSocket = new ServerSocket(cksumServerPort);
-        Socket clientSocket = null;
+        
         System.out.println("Server waiting for connection");
-        clientSocket = serverSocket.accept();
+        Socket clientSocket = serverSocket.accept();
         System.out.println("Server connected to Client " + clientSocket);
         Socket clientCksumSocket = cksumSocket.accept();
         System.out.println("Server connected to Client Checksum " + clientCksumSocket);
+        
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         InputStream clientInputStream = clientSocket.getInputStream();
+        //create buffer readers for client input and checksum
         BufferedReader in = new BufferedReader( new InputStreamReader(clientInputStream));
         BufferedReader cksumInput = new BufferedReader(new InputStreamReader(clientCksumSocket.getInputStream()));
+        //create checksum on client input stream
         CheckedInputStream cksum = new CheckedInputStream(clientInputStream, new CRC32());
         System.out.println("Get input from client");
         String inputLine;
+        //while there is input from client loop
         while ((inputLine = in.readLine()) != null)
         {
             System.out.println("Server: " + inputLine);
-            out.println(inputLine);
+            //if input is terminating 'X' character reply to client and break loop
             if(inputLine.equals("X"))
             {
                 out.println(inputLine);
                 break;
             }
+            //calculate checksum from client input
             cksum.getChecksum().update(inputLine.getBytes(), 0, inputLine.length());
-            System.out.println("Server Checksum: " + cksum.getChecksum().getValue());
-            System.out.println("Client Checksum: " + cksumInput.readLine());
+            String clientCksum = cksumInput.readLine();
+            String serverCksum = Long.toString(cksum.getChecksum().getValue());
+            System.out.println("Server Checksum: " + serverCksum);
+            System.out.println("Client Checksum: " + clientCksum);
+            //compare client and server generated checksums
+            if(clientCksum.equals(serverCksum))
+                System.out.println("Checksums Match");
+            else
+                System.out.println("Error during transmition checksums do not match");
+            //send reply to client
+            out.println(inputLine);
+            
         }
+        //close connection with client
         System.out.println("Connection Closed");
         out.close();
         in.close();
